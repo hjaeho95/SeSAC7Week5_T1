dccea50bb80e9ef80101303f09127c5a1080c4d9
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-25 20:38:03 +0900

    Feat: 데이터 struct 구성

--- SeSAC7Week5_T1/Shop/Model/Shop.swift
+++ SeSAC7Week5_T1/Shop/Model/Shop.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct Shop: Decodable {
+    let lastBuildDate: String
+    let total: Int
+    let start: Int
+    let display: Int
+    let items: [ShopItem]
+}
+
+struct ShopItem: Decodable {
+    let title: String
+    let link: String
+    let image: String
+    let lprice: String
+    let hprice: String
+    let mallName: String
+    let productId: String
+    let productType: String
+    let brand: String
+    let maker: String
+    let category1: String
+    let category2: String
+    let category3: String
+    let category4: String
+}
