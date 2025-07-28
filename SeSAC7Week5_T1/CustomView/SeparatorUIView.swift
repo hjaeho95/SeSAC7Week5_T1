390ad74ecab55c70e6bae3b967af0a44c90ca162
Jaeho <68717044+hjaeho95@users.noreply.github.com>
2025-07-28 18:02:52 +0900

    Refactor: @available(*, unavailable) 적용

--- SeSAC7Week5_T1/CustomView/SeparatorUIView.swift
+++ SeSAC7Week5_T1/CustomView/SeparatorUIView.swift
@@ -8,6 +8,7 @@ class SeparatorUIView: UIView {
         backgroundColor = .systemGray
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- SeSAC7Week5_T1/Shop/ViewController/ViewCell/ShopItemCollectionViewCell.swift
+++ SeSAC7Week5_T1/Shop/ViewController/ViewCell/ShopItemCollectionViewCell.swift
@@ -76,6 +76,7 @@ class ShopItemCollectionViewCell: UICollectionViewCell {
         configureLayout()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
